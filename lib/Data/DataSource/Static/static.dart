f1d52e104a34cfe08c75fec8fea900cc395be6e7
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-07-27 14:22:16 +0300

    onBoardingList

--- lib/Data/DataSource/Static/static.dart
+++ lib/Data/DataSource/Static/static.dart
@@ -1,7 +1,18 @@
+import 'package:elmamlouk_mall/Core/Constants/images.dart';
 import 'package:elmamlouk_mall/Data/Models/onboarding_model.dart';
 
 List<OnBoardingModel> onBoardingList = [
-  OnBoardingModel(title: "",
-   imgPath: "",
-    body: "")
+  OnBoardingModel(
+      title: "Choose Product",
+      imgPath: Images.onBoarding1,
+      body:
+          "Everything you need from home essentials and electrical appliances"),
+  OnBoardingModel(
+      title: "Add to cart",
+      imgPath: Images.onBoarding2,
+      body: "Add everything you want to your shopping cart"),
+  OnBoardingModel(
+      title: "Fast Services , Fast Shipping ",
+      imgPath: Images.onBoarding3,
+      body: "Shipping all over Egypt")
 ];
