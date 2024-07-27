2f776d4a72c71053ff092e0db1e9682cdb1bebdc
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-07-27 14:06:31 +0300

    OnBoardingModel

---
+++ lib/Data/DataSource/Static/static.dart
@@ -0,0 +1,7 @@
+import 'package:elmamlouk_mall/Data/Models/onboarding_model.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(title: "",
+   imgPath: "",
+    body: "")
+];
---
+++ lib/Data/Models/onboarding_model.dart
@@ -0,0 +1,8 @@
+class OnBoardingModel {
+  final String? title;
+  final String? imgPath;
+  final String? body;
+
+  OnBoardingModel(
+      {required this.title, required this.imgPath, required this.body});
+}
