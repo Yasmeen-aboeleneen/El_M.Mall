f91151909e8ef332ee197b72d36c7cf448ba99b4
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-07-30 01:19:28 +0300

    LanguagesScreen

--- lib/Views/Langs/languages_screen.dart
+++ lib/Views/Langs/languages_screen.dart
@@ -1,6 +1,7 @@
 import 'package:elmamlouk_mall/Core/Constants/colors.dart';
 import 'package:elmamlouk_mall/Core/Localization/change_language.dart';
 import 'package:elmamlouk_mall/Views/Widget/LangWidgets/customLangButton.dart';
+import 'package:elmamlouk_mall/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,29 +19,33 @@ class LanguagesScreen extends GetView<LocaleController> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                "titleLanguage".tr,
-                style: GoogleFonts.notoSansOlChiki(
-                    fontWeight: FontWeight.bold,
-                    fontSize: w * .055,
-                    color: kPrimary),
-              ),
+            Text(
+              textDirection: TextDirection.ltr,
+              "titleLanguage".tr,
+              style: GoogleFonts.notoSansOlChiki(
+                  fontWeight: FontWeight.bold,
+                  fontSize: w * .055,
+                  color: kPrimary),
             ),
             SizedBox(
               height: h * .04,
             ),
             Customlangbutton(
               title: "اللغة العربية",
-              onpressed: () {},
+              onpressed: () {
+                controller.changeLang("ar");
+                Get.toNamed(AppRoutes.onBoardingScreen);
+              },
             ),
             SizedBox(
               height: h * .01,
             ),
             Customlangbutton(
               title: "English",
-              onpressed: () {},
+              onpressed: () {
+                controller.changeLang("en");
+                Get.toNamed(AppRoutes.onBoardingScreen);
+              },
             )
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,9 @@ import 'package:elmamlouk_mall/Core/Constants/colors.dart';
 import 'package:elmamlouk_mall/Core/Localization/change_language.dart';
 import 'package:elmamlouk_mall/Core/Localization/translation.dart';
 import 'package:elmamlouk_mall/Core/Services/services.dart';
-import 'package:elmamlouk_mall/Views/Langs/languages_screen.dart';
+import 'package:elmamlouk_mall/Views/Splash/splash_screen.dart';
 import 'package:elmamlouk_mall/routes.dart';
-// import 'package:elmamlouk_mall/Views/Splash/splash_screen.dart';
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() async {
@@ -29,7 +28,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: kveryWhite,
         useMaterial3: true,
       ),
-      home: const LanguagesScreen(),
+      home: const SplashScreen(),
       routes: routes,
     );
   }
